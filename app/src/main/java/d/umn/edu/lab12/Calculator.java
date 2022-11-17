8368cc5fc9aca72b09ea370df1f0c95217d0c776
Ryanaldo34 <ryanaldo34@gmail.com>
2022-11-17 14:20:00 -0600

    Inital UI Created

---
+++ app/src/main/java/d/umn/edu/lab12/Calculator.java
@@ -0,0 +1,36 @@
+package d.umn.edu.lab12;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Calculator extends Fragment {
+
+    private CalculatorViewModel mViewModel;
+
+    public static Calculator newInstance() {
+        return new Calculator();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_calculator, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CalculatorViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
